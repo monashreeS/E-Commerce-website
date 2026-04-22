78f0a784a6bfc8b1f045b62356c6ab39be3b316f
Monashree S <smonashree14@gmail.com>
2026-04-22 23:14:25 +0530

    created login and signup pages

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,24 @@
 const express = require('express');
-const app= express();
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const AuthRouter = require('./routes/AuthRouter');
+const ProductRouter = require('./routes/ProductRouter');
+
 require('dotenv').config();
-const PORT = process.env.PORT||8080;
+require('./Models/db');
+const PORT = process.env.PORT || 8080;
 
-app.listen(PORT,()=>{
-  console.log(`Server is running on ${PORT}`)
+app.get('/ping', (req, res) => {
+    res.send('PONG');
+});
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/auth', AuthRouter);
+app.use('/products', ProductRouter);
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
 })
-
-
-
-
--- backend/models/db.js
+++ backend/models/db.js
@@ -1,1 +1,8 @@
-const mongoose= require('mongoose');`1`
+const mongoose= require('mongoose');
+const mongo_url=process.env.MONGO_CONN;
+mongoose.connect(mongo_url)
+  .then(()=>{
+    console.log('MongoDB Connected')
+  }).catch((err)=>{
+    console.log('MpngoDB Connection Error:',err);
+  })
